c146dfce3f7a56113ce9459e643e0e8ec1d5ab70
jhs512 <jangka512@gmail.com>
2022-04-02 17:41:43 +0900

    existsByEmail 메서드로 회원가입시 이메일중복체크

--- src/main/java/com/jhs/exam/jpaBoard/user/dao/UserRepository.java
+++ src/main/java/com/jhs/exam/jpaBoard/user/dao/UserRepository.java
@@ -4,5 +4,5 @@ import com.jhs.exam.jpaBoard.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByEmail(String email);
 }
