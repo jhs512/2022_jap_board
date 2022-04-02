b98cddefb4b2ee6c6f94132dc76e84236f4a0dde
jhs512 <jangka512@gmail.com>
2022-04-02 17:37:42 +0900

    이메일이 동일한 회원이 생기지 않도록 DB수준에서 막기

--- src/main/java/com/jhs/exam/jpaBoard/user/dao/UserRepository.java
+++ src/main/java/com/jhs/exam/jpaBoard/user/dao/UserRepository.java
@@ -4,4 +4,5 @@ import com.jhs.exam.jpaBoard.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
