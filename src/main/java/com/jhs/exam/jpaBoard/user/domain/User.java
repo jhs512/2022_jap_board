1b3cb7f4d3fa850aaaa0d1c292e3ef79f8b5cad2
jhs512 <jangka512@gmail.com>
2022-04-02 16:25:16 +0900

    비밀번호는 json 대상에서 제외

--- src/main/java/com/jhs/exam/jpaBoard/user/domain/User.java
+++ src/main/java/com/jhs/exam/jpaBoard/user/domain/User.java
@@ -1,9 +1,13 @@
 package com.jhs.exam.jpaBoard.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,5 +21,6 @@ public class User {
     private LocalDateTime updateDate;
     private String email;
     private String name;
+    @JsonIgnore
     private String password;
 }
