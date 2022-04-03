f120e98925dd03a458233e82195b8dae07adb292
jhs512 <jangka512@gmail.com>
2022-04-03 15:34:26 +0900

    로그인

--- src/main/java/com/jhs/exam/jpaBoard/user/controller/UserController.java
+++ src/main/java/com/jhs/exam/jpaBoard/user/controller/UserController.java
@@ -15,16 +15,47 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @RequestMapping("doLogin")
+    @ResponseBody
+    public String doLogin(String email, String password) {
+        if (email == null || email.trim().length() == 0) {
+            return "이메일을 입력해주세요.";
+        }
+
+        email = email.trim();
+
+        User user = userRepository.findByEmail(email).get();
+
+        if (user == null) {
+            return "일치하는 회원이 존재하지 않습니다.";
+        }
+
+        if (password == null || password.trim().length() == 0) {
+            return "비밀번호를 입력해주세요.";
+        }
+
+        password = password.trim();
+
+        System.out.println("user.getPassword() : " + user.getPassword());
+        System.out.println("password : " + password);
+
+        if (user.getPassword().equals(password) == false) {
+            return "비밀번호가 일치하지 않습니다.";
+        }
+
+        return "%s님 환영합니다.".formatted(user.getName());
+    }
+
     @RequestMapping("doJoin")
     @ResponseBody
     public String doJoin(String name, String email, String password) {
-        if ( name == null || name.trim().length() == 0 ) {
+        if (name == null || name.trim().length() == 0) {
             return "이름을 입력해주세요.";
         }
 
         name = name.trim();
 
-        if ( email == null || email.trim().length() == 0 ) {
+        if (email == null || email.trim().length() == 0) {
             return "이메일을 입력해주세요.";
         }
 
@@ -32,15 +63,15 @@ public class UserController {
 
         boolean existsByEmail = userRepository.existsByEmail(email);
 
-        if ( existsByEmail ) {
+        if (existsByEmail) {
             return "입력하신 이메일(%s)은 이미 사용중입니다.".formatted(email);
         }
 
-        if ( password == null || password.trim().length() == 0 ) {
+        if (password == null || password.trim().length() == 0) {
             return "비밀번호를 입력해주세요.";
         }
 
-        password = email.trim();
+        password = password.trim();
 
         User user = new User();
         user.setRegDate(LocalDateTime.now());
