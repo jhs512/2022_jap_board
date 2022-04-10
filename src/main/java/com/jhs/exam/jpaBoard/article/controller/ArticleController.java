9194c0552476e8a7ae077ff6bebc05bba0db7d84
jhs512 <jangka512@gmail.com>
2022-04-10 17:35:35 +0900

    게시물 수정폼에서 권한체크

--- src/main/java/com/jhs/exam/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/jhs/exam/jpaBoard/article/controller/ArticleController.java
@@ -164,9 +164,29 @@ public class ArticleController {
     }
 
     @RequestMapping("modify")
-    public String showModify(long id, Model model) {
+    public String showModify(long id, HttpSession session, Model model) {
+        boolean isLogined = false;
+        long loginedUserId = 0;
+
+        if (session.getAttribute("loginedUserId") != null) {
+            isLogined = true;
+            loginedUserId = (long) session.getAttribute("loginedUserId");
+        }
+
+        if (isLogined == false) {
+            model.addAttribute("msg", "로그인 후 이용해주세요.");
+            model.addAttribute("historyBack", true);
+            return "common/js";
+        }
+
         Optional<Article> opArticle = articleRepository.findById(id);
         Article article = opArticle.get();
+
+        if (article.getUser().getId() != loginedUserId) {
+            model.addAttribute("msg", "권한이 없습니다.");
+            model.addAttribute("historyBack", true);
+            return "common/js";
+        }
 
         model.addAttribute("article", article);
 
