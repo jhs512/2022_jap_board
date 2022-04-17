20a6d37d4241e2f3038de3b308630132923e6f8e
jhs512 <jangka512@gmail.com>
2022-04-17 16:29:26 +0900

    로그아웃 후 메인페이지로 이동

--- src/main/java/com/jhs/exam/jpaBoard/user/controller/UserController.java
+++ src/main/java/com/jhs/exam/jpaBoard/user/controller/UserController.java
@@ -21,7 +21,6 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping("doLogout")
-    @ResponseBody
     public String doLogout(HttpSession session) {
         boolean isLogined = false;
 
@@ -30,12 +29,12 @@ public class UserController {
         }
 
         if (isLogined == false) {
-            return "이미 로그아웃 되었습니다.";
+            return "redirect:/";
         }
 
         session.removeAttribute("loginedUserId");
 
-        return "로그아웃 되었습니다.";
+        return "redirect:/";
     }
 
     @RequestMapping("login")
